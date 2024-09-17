85ba361969933d9fb7b1e28b064b593931bf2e46
0xDRAGOS <sdragos231@gmail.com>
2024-09-17 15:59:41 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <p className="text-amber-400 flex justify-center">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
